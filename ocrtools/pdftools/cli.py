98f7309d91cde942d9e1bcc1af76d1f861124ae8
William Thompson <will.k.t@gmail.com>
2023-09-20 17:32:20 -0500

    updates

--- ocrtools/pdftools/cli.py
+++ ocrtools/pdftools/cli.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from rich import console as cons
 from typing import Dict, Optional
 from PIL import Image as im
-
+import ocrtools.pdftools.functions as F
 
 # -----------------------------------------------------------------------------
 app = typer.Typer()
@@ -16,7 +16,10 @@ CONFIG: Dict[str, str] = {}
 def extract_pages(
     pdf_file: Path = typer.Argument(..., help="Path to input PDF file"),
     outdir: Path = typer.Option(Path("."), help="Path to output page image files"),
+    start: Optional[int] = typer.Option(None, help="Starting page number"),
+    end: Optional[int] = typer.Option(None, help="Ending page number"),
 ):
     console.print(f"extracting pages from: {pdf_file}")
     outdir.mkdir(parents=True, exist_ok=True)
+    F.extract_pages(pdf_file, outdir, start, end)
     
--- ocrtools/pdftools/functions.py
+++ ocrtools/pdftools/functions.py
@@ -1,2 +1,20 @@
 from pathlib import Path
+from typing import Iterable
+import fitz
 
+
+def extract_pages(pdf: Path, outdir: Path, page_start: int, page_end: int):
+    doc = fitz.open(pdf)
+    
+    if page_start is None:
+        page_start = 0
+    if page_end is None:
+        page_end = len(doc)
+    page_end = min(page_end + 1, len(doc))
+    assert page_start <= page_end, "page_start must be less than or equal to page_end"
+
+    mat = fitz.Matrix(3, 3)
+    for i in range(page_start, page_end):
+        page = doc[i-1]
+        pix = page.get_pixmap(matrix = mat)
+        pix.save(outdir/f"page-{str(page.number+1).zfill(4)}.png")
